c5e90a95d951a0f683c26384d337f60fb3577552
Adrian Jankowski <jankowski.adrian13@gmail.com>
2020-12-06 18:57:13 +0100

    Added solution - part 2, day 4.

--- src/main/java/SolutionDay4.java
+++ src/main/java/SolutionDay4.java
@@ -1,9 +1,6 @@
 import utils.DataGetter;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SolutionDay4 {
 
@@ -18,16 +15,16 @@ public class SolutionDay4 {
                 passportValues.containsKey("pid");
     }
 
-//    public boolean isStrictValid(List<String> input) {
-//        Map<String, String> passportValues = getPassportData(input);
-//        return passportValues.containsKey("byr") && passportValues.get("byr").matches("(19[2-8][0-9]|199[0-9]|200[0-2])") &&
-//                passportValues.containsKey("iyr") && passportValues.get("iyr").matches("(201[0-9]|2020)") &&
-//                passportValues.containsKey("eyr") && passportValues.get("eyr").matches("(202[0-9]|2030)")
-//                passportValues.containsKey("hgt") &&
-//                passportValues.containsKey("hcl") &&
-//                passportValues.containsKey("ecl") &&
-//                passportValues.containsKey("pid");
-//    }
+    public boolean isStrictValid(List<String> input) {
+        Map<String, String> passportValues = getPassportData(input);
+        return passportValues.containsKey("byr") && passportValues.get("byr").matches("(19[2-8][0-9]|199[0-9]|200[0-2])") &&
+                passportValues.containsKey("iyr") && passportValues.get("iyr").matches("(201[0-9]|2020)") &&
+                passportValues.containsKey("eyr") && passportValues.get("eyr").matches("(202[0-9]|2030)") &&
+                passportValues.containsKey("hgt") && checkHeight(passportValues.get("hgt")) &&
+                passportValues.containsKey("hcl") && passportValues.get("hcl").matches("(#[a-f0-9]{6})") &&
+                passportValues.containsKey("ecl") && checkEyeColor(passportValues.get("ecl")) &&
+                passportValues.containsKey("pid") && passportValues.get("pid").matches("([0-9]{9})");
+    }
 
     private Map<String, String> getPassportData(List<String> input) {
         Map<String, String> passportValues = new HashMap<>();
@@ -41,12 +38,21 @@ public class SolutionDay4 {
         return passportValues;
     }
 
-//    private boolean checkHeight(String input) {
-//        if(input.contains("cm"))
-//            return input.matches("(1[5-8][0-9]|19[0-3])cm");
-//        else
-//            return input.matches("(59|6[0-9]|7[0-6])");
-//    }
+    private boolean checkHeight(String input) {
+        if (input.contains("cm"))
+            return input.matches("(1[5-8][0-9]|19[0-3])cm");
+        else
+            return input.matches("(59|6[0-9]|7[0-6])in");
+    }
+
+    private boolean checkEyeColor(String input) {
+        List<String> eyeColors = Arrays.asList("amb", "blu", "brn", "gry", "grn", "hzl", "oth");
+
+        for (String eyeColor : eyeColors)
+            if (input.equals(eyeColor))
+                return true;
+        return false;
+    }
 
     public static void main(String... args) {
         SolutionDay4 solution = new SolutionDay4();
@@ -55,16 +61,20 @@ public class SolutionDay4 {
         List<String> allLines = dataGetter.getLinesFromFile("data/day4.txt");
         List<String> personData = new ArrayList<>();
         int validCnt = 0;
+        int strictValidCnt = 0;
 
         for (String line : allLines) {
             if (line.length() == 0) {
                 if (solution.isValid(personData)) validCnt++;
+                if (solution.isStrictValid(personData)) strictValidCnt++;
                 personData.clear();
             } else
                 personData.add(line);
         }
         if (solution.isValid(personData)) validCnt++;
+        if (solution.isStrictValid(personData)) strictValidCnt++;
 
         Solution.printAnswer(Solution.Answer.Answer_1, validCnt);
+        Solution.printAnswer(Solution.Answer.Answer_2, strictValidCnt);
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -80,5 +80,42 @@ public class SolutionTest {
                 "hcl:#cfa07d byr:1929"
                 )),
                 false);
+
+        Assert.assertEquals(solution.isStrictValid(Arrays.asList(
+                "eyr:1972 cid:100",
+                "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926")),
+                false);
+        Assert.assertEquals(solution.isStrictValid(Arrays.asList(
+                "iyr:2019",
+                "hcl:#602927 eyr:1967 hgt:170cm",
+                "ecl:grn pid:012533040 byr:1946")),
+                false);
+        Assert.assertEquals(solution.isStrictValid(Arrays.asList(
+                "hcl:dab227 iyr:2012",
+                "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277")),
+                false);
+        Assert.assertEquals(solution.isStrictValid(Arrays.asList(
+                "hgt:59cm ecl:zzz",
+                "eyr:2038 hcl:74454a iyr:2023",
+                "pid:3556412378 byr:2007")),
+                false);
+
+        Assert.assertEquals(solution.isStrictValid(Arrays.asList(
+                "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980",
+                "hcl:#623a2f")),
+                true);
+        Assert.assertEquals(solution.isStrictValid(Arrays.asList(
+                "eyr:2029 ecl:blu cid:129 byr:1989",
+                "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm")),
+                true);
+        Assert.assertEquals(solution.isStrictValid(Arrays.asList(
+                "hcl:#888785",
+                "hgt:164cm byr:2001 iyr:2015 cid:88",
+                "pid:545766238 ecl:hzl",
+                "eyr:2022")),
+                true);
+        Assert.assertEquals(solution.isStrictValid(Arrays.asList(
+                "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719")),
+                true);
     }
 }
